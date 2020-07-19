317d4d7defe8dbd9a0604de99f06d9390fbefe2b
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-19 15:41:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,4 +4,22 @@ def join_nested_strings(src)
   # Easier way
   # answer = src.flatten.select { |ele| ele.is_a? String }
   # answer.join(" ")
+  
+  #requested way
+  row_index = 0
+  new_str = ""
+  while row_index < src.length
+  row = src[row_index]
+  ele_index=0
+    while ele_index < row.length
+      ele = row[ele_index]
+      if ele.is_a? String
+        new_str+= ele
+      end
+    ele_index+=1
+    end
+  
+  row_index +=1
+  end
+  new_str
 end
